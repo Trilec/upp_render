0fff482082b60ddacbf14aa41b878b21438e60c2
Curtis <Trilec@users.noreply.github.com>
2026-08-17 12:43:21 +1200

    TASK-009C: replace S16 replay hooks with default display-list hook

--- render/GpuCtrl/GpuCtrlTestHooks.h
+++ render/GpuCtrl/GpuCtrlTestHooks.h
@@ -5,26 +5,8 @@
 namespace Upp {
 namespace GpuCtrlTestHooks {
 
-struct ReplayColor {
-	float red = 0.0f;
-	float green = 0.0f;
-	float blue = 0.0f;
-	float alpha = 1.0f;
-};
-
-struct ReplayFillRect : Moveable<ReplayFillRect> {
-	Rect rect = Rect(0, 0, 0, 0);
-	ReplayColor color;
-};
-
-struct ReplayResult {
-	ReplayColor background;
-	Vector<ReplayFillRect> fill_rects;
-	String error;
-};
-
-bool ReplayDisplayList(const UiDisplayList& list, ReplayResult& out);
-bool BuildDefaultFrame(Size size, ReplayResult& out);
+// Builds the same immutable Stage-4 scene that the live GpuCtrl presents.
+bool BuildDefaultFrame(Size size, UiDisplayList& list, Rgba8& background, String& error);
 
 } // namespace GpuCtrlTestHooks
 } // namespace Upp
